e9eec3a0dfb6ae2060e3a6a0b1191346aa00f282
natan8bis <natan8bis@gmail.com>
2026-08-03 22:43:58 -0400

    estudo de API no neovim

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json()); //recebe o json e coloca tudo em req.body
+
+app.get("/", (req, res) => {
+  res.json({
+    mensagem: "minha primeira API",
+    autor: "Natan",
+    version: "1.0",
+  });
+});
+
+app.get("/sobre", (req, res) => {
+  res.json({
+    projeto: "minha api",
+    linguagem: "javascript",
+    framework: "expres",
+  });
+});
+
+//essa eh uma rota dinamica. recupera o parametro passado na url
+//usado quando queremos usar um recurso especifico
+app.get("/usuarios/:id", (req, res) => {
+  const id = req.params.id;
+
+  res.json({
+    mensagem: "Usuário encontrado.",
+    id: id,
+  });
+});
+
+//usado quando queremos filtrar ou ordenar
+// /buscar?nome=natan
+app.get("/buscar", (req, res) => {
+  res.json({
+    //acessar query que nao existem retorna undefined
+    pesquisa: req.query.nome,
+  });
+});
+
+// curl -X POST http://localhost:3000/usuarios -H "Content-Type: application/json" -d '{"nome":"Natan","idade":20}'
+
+app.post("/usuarios", (req, res) => {
+  res.json({
+    mensagem: "usuario salvo com sucesso",
+    usuario: req.body,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando em http://localhost:3000");
+});
